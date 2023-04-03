4847e69b24e8ac784e66915af270feed534c5cb6
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-04-03 21:27:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
-
 import pygame
 
 # Initialize Pygame screen
 pygame.init()
 
 # Create screen
-screen = pygame.display.set_mode((900, 900))
+screen = pygame.display.set_mode((900, 1000))
 
 bg = pygame.image.load('background.png')
 pygame.display.set_caption('Tic Tac Toe')
@@ -54,9 +53,6 @@ def check_click(x, y, player):
             if array[8] == "-":
                 array[8] = player
 
-def draw_win_line():
-    line_x_start
-
 
 def check_win(player):
     # checking rows
@@ -66,7 +62,7 @@ def check_win(player):
             line_x_end = coordinates[int][0] + 800
             line_y = coordinates[int][1] + 130
             pygame.draw.line(screen,
-                             color=(150, 179, 113),
+                             color=(255, 255, 255),
                              start_pos=[line_x_start, line_y],
                              end_pos=[line_x_end, line_y],
                              width=20)
@@ -79,27 +75,29 @@ def check_win(player):
             line_y_start = coordinates[int][1]
             line_y_end = coordinates[int][1] + 800
             pygame.draw.line(screen,
-                             color=(150, 179, 113),
+                             color=(255, 255, 255),
                              start_pos=[line_x, line_y_start],
                              end_pos=[line_x, line_y_end],
                              width=20)
             return True
 
-
     # checking diagonals
     if array[0] != '-' and array[0] == array[4] and array[4] == array[8]:
-        line_x = coordinates[int][0] + 130
-        line_y_start = coordinates[int][1]
-        line_y_end = coordinates[int][1] + 800
         pygame.draw.line(screen,
-                         color=(150, 179, 113),
-                         start_pos=[line_x, line_y_start],
-                         end_pos=[line_x, line_y_end],
+                         color=(255, 255, 255),
+                         start_pos=[20, 20],
+                         end_pos=[820, 820],
                          width=20)
-        return True, int
+        return True
+
     elif array[2] != '-' and array[2] == array[4] and array[4] == array[6]:
         print(f'{player} wins')
-        return True,
+        pygame.draw.line(screen,
+                         color=(255, 255, 255),
+                         start_pos=[880, 20],
+                         end_pos=[25, 880],
+                         width=40)
+        return True
 
     # Draw
     remaining_moves = 9
@@ -107,20 +105,38 @@ def check_win(player):
         if array[i] != '-':
             remaining_moves -= 1
             if remaining_moves == 0:
-                print("It's a draw!")
-                return True
+                return remaining_moves
 
 
-player = "x"
+def draw_shapes():
+    for i, x in enumerate(array):
+        if array[i] == 'X':
+            screen.blit(img_x, (coordinates[i][0], coordinates[i][1]))
+        elif array[i] == "O":
+            screen.blit(img_o, (coordinates[i][0], coordinates[i][1]))
+
+
+font1 = pygame.font.SysFont('arialblack', 40)
+font2 = pygame.font.SysFont('arialblack', 25)
+
+TEXT_COL = (250, 250, 250)
+
+
+def reset_game(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
+player = "X"
 game_on = True
 
-screen.fill((255, 255, 255))
+screen.blit(bg, (0, 0))
+
 while True:
-    screen.blit(bg, (0, 0))
+    draw_shapes()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if game_on:
                 x = pygame.mouse.get_pos()[0]
@@ -129,17 +145,21 @@ while True:
 
                 if check_win(player):
                     game_on = False
-
+                    reset_game(f'Game over, {player} won!', font1, TEXT_COL, 250, 875)
+                    reset_game('Press "R" to retry or "Q" to quit', font2, TEXT_COL, 237, 925)
+                elif check_win(player) == 0:
+                    reset_game("It's a draw.", font1, TEXT_COL, 325, 875)
+                if player == "X":
+                    player = "O"
                 else:
-                    if player == "x":
-                        player = "o"
-                    else:
-                        player = "x"
-
-    for i, x in enumerate(array):
-        if array[i] == 'x':
-            screen.blit(img_x, (coordinates[i][0], coordinates[i][1]))
-        elif array[i] == "o":
-            screen.blit(img_o, (coordinates[i][0], coordinates[i][1]))
+                    player = "X"
+        elif event.type == pygame.KEYDOWN and not game_on:
+            if event.key == pygame.K_r:
+                array = ['-', '-', '-',
+                         '-', '-', '-',
+                         '-', '-', '-']
+                game_on = True
+                screen.blit(bg, (0, 0))
+                print(array)
 
     pygame.display.update()
